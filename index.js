c374ab6c52db306dd4c7f6d4de7f0cb82659b19a
Jefferson Escobar <jeffersoncamilo.esc@gmail.com>
2023-04-07 17:06:06 -0500

    conflictos API

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const API_KEY = '9891a7811e8077fee1f13a421a2fed57'
 
-const fetchData = position => {     //ME TRAE LA POSICION DEL USUARIO.   //FECTHDATA = OBETENER LA DATA DEL CLIMA.
-    const { latitud, longitude } = position.coords;  //OBTENER LA INFORMACION DE POSITION = LONGITUDE & LATITUD. //VAMOS A UTILIZARLAS PARA HACER LA LLAMADA A LA API.
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitude}&appid=${API_KEY}`)
-        .then(response => console.log(response));   //CONSOLE.LOG PARA MIRAR QUE DATA NOS DEVUELVE RESPONSE ES LO IMPORTANTE.
-    console.log(position);
+const fetchData = position => {     //ME TRAE LA POSICIÓN DEL USUARIO.   //FECTHDATA = OBETENER LA DATA DEL CLIMA.
+    const { latitude , longitude } = position.coords;  //OBTENER LA INFORMACION DE POSITION = LONGITUDE & LATITUD. //VAMOS A UTILIZARLAS PARA HACER LA LLAMADA A LA API.
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then(response => response.json())   //CONSOLE.LOG PARA MIRAR QUE DATA NOS DEVUELVE RESPONSE ES LO IMPORTANTE.
+        .then(data => setWeatherData(data));
+}
+
+const setWeatherData = data => {
+    console.log(data);                          //LO QUE VAMOS HACER AQUÍ ADENTRO ES PONER TODA LA INFO QUE RECIBIMOS EN EL HTML.
+    const weatherData = {                       //UNA VARIABLE PARA JUNTAR TODA LA INFORMACIÓN.
+        location: date.name,    
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pleassure: data.main.pressure,
+        temperature: data.main.temp,
+        date: 'date',
+    }                            
 }
 
 const onLoad = () => {
