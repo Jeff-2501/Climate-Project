2ee7a742ba3db83129505b037f30fb74fe4f0562
Jefferson Escobar <jeffersoncamilo.esc@gmail.com>
2023-04-08 06:48:33 -0500

    conflictos API2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Climate-Project
 Proyecto basico para practicar HTML, CSS Y JS
+
+llamado a la API https://openweathermap.org/guide para obtener lo datos del clima.
--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const API_KEY = '9891a7811e8077fee1f13a421a2fed57'
+const API_KEY = '9891a7811e8077fee1f13a421a2fed57';
 
 const fetchData = position => {     //ME TRAE LA POSICIÓN DEL USUARIO.   //FECTHDATA = OBETENER LA DATA DEL CLIMA.
     const { latitude , longitude } = position.coords;  //OBTENER LA INFORMACION DE POSITION = LONGITUDE & LATITUD. //VAMOS A UTILIZARLAS PARA HACER LA LLAMADA A LA API.
-    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
         .then(response => response.json())   //CONSOLE.LOG PARA MIRAR QUE DATA NOS DEVUELVE RESPONSE ES LO IMPORTANTE.
-        .then(data => setWeatherData(data));
+        .then(data => setWeatherData(data))
 }
 
 const setWeatherData = data => {
     console.log(data);                          //LO QUE VAMOS HACER AQUÍ ADENTRO ES PONER TODA LA INFO QUE RECIBIMOS EN EL HTML.
     const weatherData = {                       //UNA VARIABLE PARA JUNTAR TODA LA INFORMACIÓN.
-        location: date.name,    
+        location: data.name,    
         description: data.weather[0].main,
         humidity: data.main.humidity,
         pleassure: data.main.pressure,
         temperature: data.main.temp,
-        date: 'date',
+        date: 'data',
     }                            
+
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];                                                                        //VAMOS A SETIAR LA INFO EN NUESTRO HTML
+    });
+
 }
 
 const onLoad = () => {
